f6f3239c76e2bc2738bdcece964a1e9a4b034ff1
BATTA_MAHENDRA <battamahendra26@gmail.com>
2026-03-13 18:02:05 +0530

    Added few pics for micro service architecture

--- TestEclipse/src/main/java/Testing/TestEclipse/TestEclipseApplication.java
+++ TestEclipse/src/main/java/Testing/TestEclipse/TestEclipseApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableCaching
 public class TestEclipseApplication {
 
